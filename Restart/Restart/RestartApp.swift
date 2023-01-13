c0bdcab0f767e683eb7fff72349e84a857e90eeb
SebiC98 <sebastiancioata98@gmail.com>
2023-01-13 23:06:37 +0200

    First commit

---
+++ Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Sebastian Cioată on 13.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
